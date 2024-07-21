3efa51cfe90bbe3e19ab1c41aa6610c2e040a0de
Alberto Fanton <alberto.fanton@protonmail.com>
2024-07-21 19:35:49 +0200

    Bump everything to support 9.0.0

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     cardano-node.url = "github:intersectmbo/cardano-node";
     cardano-configurations = {
-      url = "github:input-output-hk/cardano-configurations?rev=fa19a63c9a2050940583b10e9f39ed02e3cf306f";
+      url = "github:input-output-hk/cardano-configurations?rev=de80edfd569d82d5191d2c6103834e700787bb2d";
       flake = false;
     };
     devshell.url = "github:numtide/devshell";
@@ -11,9 +11,9 @@
     dream2nix.url = "github:nix-community/dream2nix";
     flake-parts.url = "github:hercules-ci/flake-parts";
     # Currently the version should match
-    mithril-preview.url = "github:input-output-hk/mithril/2418.1";
+    mithril-preview.url = "github:input-output-hk/mithril/2428.0";
     # Should work for both preprod & mainnet
-    mithril.url = "github:input-output-hk/mithril/2418.1";
+    mithril.url = "github:input-output-hk/mithril/2428.0";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
     agenix.url = "github:ryantm/agenix";
