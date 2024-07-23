e5fb10dabb77f1a847a3faba2b62f9be7b37d47e
Alberto Fanton <alberto.fanton@protonmail.com>
2024-07-23 20:59:02 +0200

    Restore kupo watcher

--- cardanow-ts/kupo-watcher.ts
+++ cardanow-ts/kupo-watcher.ts
@@ -1,3 +1,14 @@
+import { kupoPort } from './config';
+
 export default async () => {
-  return true;
+  return fetch(`http://localhost:${kupoPort}/health`, { headers: {'Accept': 'application/json'} })
+  .then((res) => res.json())
+    .then((result) => {
+      console.log(`Kupo response: ${JSON.stringify(result)}`)
+      return result.most_recent_checkpoint != null && result.most_recent_checkpoint === result.most_recent_node_tip
+    })
+  .catch((e) => {
+    console.error(e)
+    return false
+  })
 }
