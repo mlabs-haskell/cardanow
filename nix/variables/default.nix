e426daca0fa5baa0a6fd60119b263735f574a83e
Alberto Fanton <alberto.fanton@protonmail.com>
2024-09-19 16:22:20 +0200

    Fix comments

--- nix/variables/default.nix
+++ nix/variables/default.nix
@@ -18,7 +18,5 @@ rec {
   PGUSER = "postgres";
   PGHOST = "0.0.0.0";
   OTEL_EXPORTER_OTLP_ENDPOINT = "localhost:4317";
-  # TODO will be deprecated when pushgateway will be 
   PUSHGATEWAY_URL = "localhost:9094";
 }
-
