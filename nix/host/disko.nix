d62f212faace1c831cef998cdd4d41cb34547af5
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-09-27 14:17:07 +0200

    Use new disko interface

--- nix/host/disko.nix
+++ nix/host/disko.nix
@@ -3,54 +3,29 @@
 
   disko.devices = {
     disk = {
-      x = {
+      nvme = {
         type = "disk";
-        device = "/dev/nvme0n1";
+        device = "/dev/disk/by-id/nvme-SAMSUNG_MZVL22T0HBLB-00B00_S677NX0T101318";
         content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
-              name = "ESP";
-              start = "0";
-              end = "960MiB";
-              fs-type = "fat32";
-              bootable = true;
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
               };
-            }
-            {
-              name = "zfs";
-              start = "1GiB";
+            };
+            zfs = {
               end = "100%";
               content = {
                 type = "zfs";
                 pool = "zroot";
               };
-            }
-          ];
-        };
-      };
-      y = {
-        type = "disk";
-        device = "/dev/nvme1n1";
-        content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
-              name = "zfs";
-              start = "1GiB";
-              end = "100%";
-              content = {
-                type = "zfs";
-                pool = "zroot";
-              };
-            }
-          ];
+            };
+          };
         };
       };
     };
